059500fc8d2f4f3e8ba7f0a02186780a8ca5b579
artu <sp.arturo@gmail.com>
2023-05-17 19:09:39 -0600

    Update sim_beta.R

--- R/sim_beta.R
+++ R/sim_beta.R
@@ -259,13 +259,13 @@ sim_beta <- function(simH0, simHa, n, m, k= 50, alpha = 0.05,
   nn <- resultsHa[,3] * resultsHa[,4]
 
   if(useParallel){
-    result1 <- foreach::foreach(i=1:NN, .combine = rbind, .packages = "ecocbo") %dopar% {
+    result1 <- foreach::foreach(i=1:NN, .combine = rbind) %dopar% {
       balanced_sampling(i, Y, mm, nn, YPU,
                         H0Sim, HaSim, resultsHa,
                         transformation, method)
     }
   } else {
-    result1 <- foreach::foreach(i=1:NN, .combine = rbind, .packages = "ecocbo") %do% {
+    result1 <- foreach::foreach(i=1:NN, .combine = rbind) %do% {
       balanced_sampling(i, Y, mm, nn, YPU,
                         H0Sim, HaSim, resultsHa,
                         transformation, method)
