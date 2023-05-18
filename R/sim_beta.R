bdc6f419ee98dc185e61a26e6359e0476de6c9e9
artu <sp.arturo@gmail.com>
2023-05-17 18:57:43 -0600

    Helper functions (permanova_oneway, balanced_sampling) are moved inside the main sim_beta function
    This was done to check if the package would pass the win test

--- R/sim_beta.R
+++ R/sim_beta.R
@@ -81,7 +81,107 @@
 sim_beta <- function(simH0, simHa, n, m, k= 50, alpha = 0.05,
                      transformation = "none", method = "bray", dummy = FALSE,
                      useParallel = TRUE, nCores = NULL){
-  # Check the inputs ----
+  # Helper functions ----
+
+  permanova_oneway <- function(x, factEnv, type = "P", method = "bray", transformation = "none"){
+
+    # Helper functions ----
+    ## Sum of Squares using Huygen theorem ----
+    SS <- function (d) {
+      ss <- numeric(2)
+      ss[1] <- dim(as.matrix(d))[1]
+      ss[2] <- sum(d^2)/ss[1]
+      return(ss)
+    }
+
+    ## PERMANOVA function ----
+    pseudoF_P <- function(x, factEnv, method = "bray", transformation = "none"){
+      if (transformation == "square root") {
+        x.t <- sqrt(x)
+        rm(x)
+        d <- vegan::vegdist(x.t, method = method)
+      }
+      if (transformation == "fourth root") {
+        x.t <- sqrt(sqrt(x))
+        rm(x)
+        d <- vegan::vegdist(x.t, method = method)
+      }
+      if (transformation == "Log (X+1)") {
+        x.t <- log(x + 1)
+        rm(x)
+        d <- vegan::vegdist(x.t, method = method)
+      }
+      if (transformation == "P/A") {
+        x.t <- 1 * (x > 0)
+        rm(x)
+        d <- vegan::vegdist(x.t, method = method, binary = TRUE)
+      }
+      if (transformation == "none") {
+        x.t <- x
+        rm(x)
+        d <- vegan::vegdist(x.t, method = method)
+      }
+
+      TSS <- SS(d)[2]
+
+      # Size for labels
+      nlev <- nlevels(as.factor(factEnv))
+
+      # Calculate the SS for residuals
+      lista <- split(as.data.frame(x.t), factEnv)
+      dimxt <- dim(x.t)
+      vdist <- lapply(lista, vegan::vegdist, method = method)
+      SSi <- lapply(vdist, SS)
+      SSi <- array(unlist(SSi), dim = c(1,2,nlev))
+
+      # Calculate denominators
+      denA <- nlev - 1
+      denR <- dimxt[1] - nlev
+
+      # Results
+      RSS <- sum(SSi[,2,])
+      ASS <- abs(TSS - RSS)
+      AMS <- (ASS/denA)
+      RMS <- (RSS/denR)
+      Fobs <- AMS/RMS
+      Fobs <- data.frame(ASS, RSS, TSS, denA, denR, AMS, RMS, Fobs)
+      return(Fobs)
+    }
+
+    # Main function permanova ----
+    Results <- pseudoF_P(x, factEnv, method, transformation)[1,c(6,7,8)]
+    Fobs <- Results
+    return(Fobs)
+  }
+
+  balanced_sampling <- function(i, Y, mm, nn, YPU, H0Sim, HaSim, resultsHa, transformation, method){
+    # main function sampling ----
+    # Get the samples index
+    sel <- sampling::balancedtwostage(Y, selection = 1, m = mm[i],
+                                      n = nn[i], PU = YPU, FALSE)
+    ones <- which(sel[,1] %in% 1)
+    y0 <- H0Sim[ones,,resultsHa[i,1]]
+    ya <- HaSim[ones,,resultsHa[i,1]]
+    yHa <- dim(y0)[2] - 2
+
+    # Apply PERMANOVA to get F and mean squares
+    result1 <- permanova_oneway(y0[, 1:yHa], y0[,yHa+2],
+                                transformation = transformation, method = method)
+    result2 <- permanova_oneway(ya[, 1:yHa], y0[,(yHa+2)],
+                                transformation = transformation, method = method)
+    result0 <- matrix(nrow = 1, ncol = 4)
+    colnames(result0) <- c("Fobs", "Fobs", "AMS", "RMS")
+
+    # Gather the results and return
+    result0[,1] <- result1[,3]
+    result0[,2] <- result2[,3]
+    result0[,3] <- result2[,1]
+    result0[,4] <- result2[,2]
+    return(result0)
+  }
+
+  # Main function sim_beta ----
+  ## Check the inputs ----
 
   if(useParallel){
     if(is.null(nCores)){
@@ -151,7 +251,7 @@ sim_beta <- function(simH0, simHa, n, m, k= 50, alpha = 0.05,
                            "pseudoFH0", "pseudoFHa",
                            "AMSHa", "RMSHa")
 
-  # Loop to calculate pseudoF ----
+  ## Loop to calculate pseudoF ----
   # Loop parameters
   Y <- cbind(1:(N * sites))
   YPU <- as.numeric(gl(sites, N))
@@ -179,7 +279,7 @@ sim_beta <- function(simH0, simHa, n, m, k= 50, alpha = 0.05,
 
   resultsHa <- resultsHa[!is.na(resultsHa[,5] | !is.na(resultsHa[,6])),]
 
-  # Calculate power and beta ----
+  ## Calculate power and beta ----
   resultsHa <- as.data.frame(resultsHa)
   fCrit <- stats::aggregate(resultsHa[,5],
                      by = list(resultsHa$m, resultsHa$n),
@@ -217,9 +317,9 @@ sim_beta <- function(simH0, simHa, n, m, k= 50, alpha = 0.05,
   return(BetaResult)
 }
 
-#-------------------------------------------
-## S3Methods print()
-#-------------------------------------------
+#
+## S3Methods print() ----
+#
 
 #' S3Methods for Printing
 #'
